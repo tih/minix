38e1a8da64fd8a556ef65fd3151f3cebe619b3db
Tom Ivar Helbekkmo <tih@hamartun.priv.no>
2021-06-06 11:33:03 +0200

    initial b004 build framework

---
+++ minix/drivers/inmos/b004/b004.h
@@ -0,0 +1,7 @@
+#ifndef __B004_H
+#define __B004_H
+
+/** The b004, World! message. */
+#define b004_MESSAGE "b004, World!\n"
+
+#endif /* __B004_H */
--- minix/include/minix/dmap.h
+++ minix/include/minix/dmap.h
@@ -77,9 +77,10 @@
 					/* 56-63 = /dev/vnd[0-7] (vnd)	      */
 #define INPUT_MAJOR		  64	/* 64 = /dev/input (input)            */
 #define USB_BASE_MAJOR		  65	/* 65-133 = USB major range	      */
-#define PCI_MAJOR		 134	/* 134 = /dev/pci (pci)            */
+#define PCI_MAJOR		 134	/* 134 = /dev/pci (pci)               */
+#define B004_MAJOR		 135	/* 135 = /dev/b004 (transputers)      */
 
-#define NR_DEVICES   		 135	/* number of (major) devices */
+#define NR_DEVICES   		 136	/* number of (major) devices */
 
 /* Minor device numbers for memory driver. */
 #  define RAM_DEV_OLD  		   0	/* minor device for /dev/ram */
